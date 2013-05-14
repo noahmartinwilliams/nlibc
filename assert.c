933beb8a30b502f86e3dee5b586589dabad60ce5
Noah Williams <thecrazymancan@gmail.com>
2013-05-14 16:36:27 -0500

    took advantage of gcc's __STRING macro to turn the line number into a string.

--- assert.c
+++ assert.c
@@ -1,12 +1,14 @@
 #include "lfile.h"
 #include "proc.h"
 
-void __assert_fail(char *expr, char *file)
+void __assert_fail(char *expr, char *file, char *line)
 {
 	low_print("assertion failed in ");
 	low_print(file);
 	low_print(" expression: ");
 	low_print(expr);
+	low_print(" line: ");
+	low_print(line);
 	low_print("\n");
 	exit(1);
 }
--- include/assert.h
+++ include/assert.h
@@ -1,10 +1,11 @@
 #ifndef __ASSERT_H__
 #define __ASSERT_H__
 #include "stddef.h"
-extern void __assert_fail(char *expr, char *file);
+#define __STRING2(x) __STRING(x)
+extern void __assert_fail(char *expr, char *file, char *line);
 # define assert(expr)					\
   ((expr)						\
    ? (void) (0)						\
-   : __assert_fail (__STRING(expr), __FILE__))
+   : __assert_fail (__STRING(expr), __FILE__, __STRING2(__LINE__)))
 
 #endif
