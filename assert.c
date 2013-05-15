c21bab38e89e5ef3efd3e06aa5adbce56a18c14e
Noah Williams <thecrazymancan@gmail.com>
2013-05-14 19:28:51 -0500

    changed the assert string format.

--- assert.c
+++ assert.c
@@ -1,14 +1,17 @@
 #include "lfile.h"
 #include "proc.h"
-
+/*assert: assert.c:5: main: Assertion `0==1' failed.
+Aborted
+134: noah@hal-9000 ~/tmp/info$*/
 void __assert_fail(char *expr, char *file, char *line)
 {
-	low_print("assertion failed in ");
+	low_print("assert: ");
 	low_print(file);
-	low_print(" expression: ");
-	low_print(expr);
-	low_print(" line: ");
+	low_print(":");
 	low_print(line);
+	low_print(" Assertion `");
+	low_print(expr);
+	low_print("' failed\n");
 	low_print("\n");
-	exit(1);
+	exit(134);
 }
