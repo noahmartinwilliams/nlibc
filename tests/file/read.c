d9abaa568b8b0e04d058a3ff8b12f6b7e7f99f86
skynet1 <noah@mini-me.(none)>
2013-05-27 09:54:20 -0500

    added a test for reading file descriptors that don't exist.

--- tests/file/read.c
+++ tests/file/read.c
@@ -1,5 +1,6 @@
 #include "file.h"
 #include "assert.h"
+#include "errno.h"
 
 int main()
 {
@@ -7,5 +8,7 @@ int main()
 	buf[5]='\0';
 	read(0, &buf, 5);
 	assert(!strcmp(&buf, "hello"));
+
+	assert(read(42, &buf, 5)==-EBADF);
 	return 0;
 }
