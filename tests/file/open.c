69ba06396409b4f5af3db4501b7772b1f32cb9a0
skynet1 <noah@mini-me.(none)>
2013-06-05 18:51:14 -0500

    added a test for open(2)ing directories in write only mode.

--- tests/file/open.c
+++ tests/file/open.c
@@ -7,8 +7,10 @@ int main()
 	int fd=open("./tmp/xyzzy",  O_RDONLY, 00);
 	int fd2=open("./tmp/xyzzy2", O_RDONLY, 00);
 	int fd3=open("./tmp/xyzzy3", O_RDONLY, 00);
+	int fd4=open("./tmp/xyzzy4", O_WRONLY, 00);
 	assert(fd2==-ENOENT);
 	assert(fd3==-EACCES);
+	assert(fd4==-EISDIR);
 	while (1) {}
 	return 0;
 }
