104f891ad9cb6333c06236d8c245832e4542107b
skynet1 <thecrazymancan@gmail.com>
2013-06-26 11:43:38 -0500

    modified the chdir(2) test to take into account nonexistant directories.

--- tests/file/chdir.c
+++ tests/file/chdir.c
@@ -2,13 +2,15 @@
 #include "assert.h"
 #include "errno.h"
 
-int main()
+int main(int argc, char *argv[])
 {
 	char buf[50];
 	read(0, &buf, 1);
 	assert(chdir("..")==0);
 	read(0, &buf, 1);
 	assert(chdir("/")==0);
+	assert(argc >= 2);
+	assert(chdir(argv[1])==-ENOENT);
 	while (1) {}
 	return 0;
 }
