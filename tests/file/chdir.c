eda17b9476a1bccb20ea403cc2d8b762c2b19fd1
skynet1 <thecrazymancan@gmail.com>
2013-06-26 10:01:41 -0500

    modified the chdir tests to include moving to the root directory.

--- tests/file/chdir.c
+++ tests/file/chdir.c
@@ -1,9 +1,14 @@
 #include "file.h"
+#include "assert.h"
+#include "errno.h"
 
 int main()
 {
 	char buf[50];
 	read(0, &buf, 1);
-	chdir("..");
+	assert(chdir("..")==0);
+	read(0, &buf, 1);
+	assert(chdir("/")==0);
+	while (1) {}
 	return 0;
 }
