9061c734996d2d7373b37b9c1332de2488d8d9f9
skynet1 <thecrazymancan@gmail.com>
2013-06-28 13:17:37 -0500

    Fixed the mmap2-anon test to get rid of annoying warnings.

--- tests/memory/mmap2-anon.c
+++ tests/memory/mmap2-anon.c
@@ -7,8 +7,8 @@
 
 int main()
 {
-	char buf[4];
-	read(0, &buf, 1);
+	char buf;
+	read(0, (char*) &buf, 1);
 	int x=(int) mmap2(NULL, 100, 0, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	read(0, &buf, 1);
 	return 0;
