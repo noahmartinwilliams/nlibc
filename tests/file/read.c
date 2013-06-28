030024f03f1a0ae6cd2ea1eba8600d0e9df2e8e8
skynet1 <thecrazymancan@gmail.com>
2013-06-28 13:17:17 -0500

    Fixed up the read tests to get rid of annoying warnings.

--- tests/file/read.c
+++ tests/file/read.c
@@ -6,13 +6,13 @@ int main()
 {
 	char buf[5];
 	buf[5]='\0';
-	read(0, &buf, 5);
-	assert(!strcmp(&buf, "hello"));
+	read(0, (void*) &buf, 5);
+	assert(!strcmp((char*) &buf, "hello"));
 
-	assert(read(42, &buf, 5)==-EBADF);
+	assert(read(42, (char*) &buf, 5)==-EBADF);
 	int fd=open("tmp/read-mode.txt", O_WRONLY|O_APPEND|O_CREAT, 0200);
 	assert(fd!=-1);
-	assert(read(fd, &buf, 5)==-EBADF);
+	assert(read(fd, (char*) &buf, 5)==-EBADF);
 	close(fd);
 	return 0;
 }
