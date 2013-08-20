ebc96ed40f6c10cf1e74bd0309ae31d270d4f03a
skynet1 <thecrazymancan@gmail.com>
2013-08-20 09:05:28 -0500

    Added a test for the O_DIRECTORY flag for the open(2) call.

--- tests/file/open.c
+++ tests/file/open.c
@@ -8,9 +8,11 @@ int main()
 	int fd2=open("./tmp/xyzzy2", O_RDONLY, 00);
 	int fd3=open("./tmp/xyzzy3", O_RDONLY, 00);
 	int fd4=open("./tmp/xyzzy4", O_WRONLY, 00);
+	int fd5=open("./tmp/xyzzy3", O_DIRECTORY, 00);
 	assert(fd2==-ENOENT);
 	assert(fd3==-EACCES);
 	assert(fd4==-EISDIR);
+	assert(fd5==-ENOTDIR);
 	while (1) {}
 	return 0;
 }
