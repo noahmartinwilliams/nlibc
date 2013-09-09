27cc8ab9b42f1a4a9b6033a3efa3b866231696a0
skynet1 <thecrazymancan@gmail.com>
2013-09-09 08:39:52 -0500

    Added a test for open(2) calls with O_EXCL and O_CREAT on existing files.

--- tests/file/open.c
+++ tests/file/open.c
@@ -10,10 +10,12 @@ int main()
 	int fd4=open("./tmp/xyzzy4", O_WRONLY, 00);
 	int fd5=open("./tmp/xyzzy3", O_DIRECTORY, 00);
 	int fd6=open("./tmp/xyzzy5", O_CREAT|O_RDONLY, 00);
+	int fd7=open("./tmp/xyzzy", O_EXCL|O_CREAT|O_RDONLY, 00);
 	assert(fd2==-ENOENT);
 	assert(fd3==-EACCES);
 	assert(fd4==-EISDIR);
 	assert(fd5==-ENOTDIR);
+	assert(fd7==-EEXIST);
 	while (1) {}
 	return 0;
 }
