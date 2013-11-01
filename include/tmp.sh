53d53e3b827ce92657107bcc99b8ace10d456389
skynet1 <thecrazymancan@gmail.com>
2013-11-01 11:47:45 -0500

    Added a newfile function to the tmp.sh file so tests can easily create temporary files.

--- include/tmp.sh
+++ include/tmp.sh
@@ -9,3 +9,13 @@ function newdir {
 		mkdir ./tmp/$1
 	fi
 }
+
+function newfile {
+	FILE=$1
+	if [ ! -f "tmp/$(basneame $FILE)" ];
+	then
+		touch tmp/$(basename $FILE)
+	else
+		truncate --size 0 tmp/$(basename $FILE)
+	fi
+}
