07a26dae6053dea5994e221f6fb0310a6ba22d56
skynet1 <thecrazymancan@gmail.com>
2013-11-07 09:23:05 -0600

    Eliminated annoying warnings.

--- tests/memory/sbrk.c
+++ tests/memory/sbrk.c
@@ -5,8 +5,8 @@ int main()
 {
 	int end=(int) sbrk((intptr_t) 0);
 	char i[2];
-	read(0, &i, 2);
+	read(0, (char*) &i, 2);
 	end=(int) sbrk((intptr_t) 100);
-	read(0, &i, 2);
+	read(0, (char*) &i, 2);
 	return 0;
 }
