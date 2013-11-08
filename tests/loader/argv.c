07d048faf94f7f32e800070fa257604a59ae6ffa
skynet1 <thecrazymancan@gmail.com>
2013-11-08 11:09:44 -0600

    Modified test makefiles to eliminate the need to clean the main directory whenever one of the source files is updated.

--- tests/loader/argv.c
+++ tests/loader/argv.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 	int x;
 	for (x=0; x<=argc-1; x++) {
 		if (argv[x]==NULL) {
-			write(2, "argv size does not match argv size\n", 35);
+			low_error("argv size does not match argv size\n");
 			return 1;
 		}
 	}
