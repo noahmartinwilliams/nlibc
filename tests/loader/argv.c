eab9b3669e3849b81fc500ecd663d53aadd8141c
skynet1 <thecrazymancan@gmail.com>
2014-05-12 11:21:17 -0500

    Modified argv test to not rely on low_error.

--- tests/loader/argv.c
+++ tests/loader/argv.c
@@ -5,8 +5,8 @@ int main(int argc, char *argv[])
 	int x;
 	for (x=0; x<=argc-1; x++) {
 		if (argv[x]==NULL) {
-			low_error("argv size does not match argv size\n");
 			return 1;
 		}
 	}
+	return 0;
 }
