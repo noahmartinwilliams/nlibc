a76d9babcde34ee8f2a5c5110054e10c7f507f26
skynet1 <thecrazymancan@gmail.com>
2014-07-30 11:53:09 -0500

    Added a new debugging feature. Now any file that has the sticky bit set will be compiled with debugging features enabled.

--- include/config.mk
+++ include/config.mk
@@ -1,11 +1,11 @@
 LIBRARY_PATH := ${LIBRARY_PATH}:./
 CC=gcc -I $(INCLUDE) -ffreestanding -nostdlib -nodefaultlibs 
 OBJ=$$(echo "$^" | sed 's/[[:space:]\+]/\n/g' | grep '.*\.[aoc]' | sed 's/\n/ /g')
-CMP=$(CC) -c $(OBJ) -o $@
+CMP=if ( stat -c='%A' $(OBJ) | grep T 2>/dev/null >/dev/null) ; then $(CC) -gstabs -c $(OBJ) -o $@ ; else $(CC) -c $(OBJ) -o $@ ; fi
 CMB=$(CC) $(OBJ) -o $@ 
 ARC=$(shell uname -p)
 LD=ld $^ -o $@
-AS=as -I $(ASMINCLUDE) $^ -o $@
+AS=if (stat -c='%A' $^ | grep T 2>/dev/null >/dev/null ) ; then as -I $(ASMINCLUDE) -gstabs $^ -o $@ ; else as -I $(ASMINCLUDE) $^ -o $@ ; fi
 AR=ar rc $@ $^
 SHARE=ld -nostdlib -share $^ -o $@
 WRAP=$$(echo "$@" | sed 's/test-//g')
