bc8e04af69aa46c51f3d124f42a097d82f540706
skynet1 <thecrazymancan@gmail.com>
2015-03-09 11:14:49 -0500

    Got sbrk2 working for x86_64.

---
+++ arch/x86_64/memory.s
@@ -0,0 +1,56 @@
+.section .text
+
+.globl mmap2
+.type mmap2, @function
+
+mmap2:
+	pushq %rbp
+	movq %rsp, %rbp
+	.equ addr, 8
+	.equ len, 12
+	.equ prot, 16
+	.equ flags, 20
+	.equ fd, 24
+	.equ off, 28
+
+	movq addr(%rbp), %rbx
+	movq len(%rbp), %rcx
+	movq prot(%rbp), %rdx
+	movq flags(%rbp), %rsi
+	movq fd(%rbp), %rdi
+	movq off(%rbp), %rbp
+	movq $192, %rax
+	int $0x80
+
+	popq %rbp
+	ret
+
+.globl sbrk2
+.type sbrk2, @function
+sbrk2:
+	pushq %rbp
+	movq %rsp, %rbp
+	.equ addr, 8
+
+	movq addr(%rbp), %rbx
+	movq $45, %rax
+	int $0x80
+
+	popq %rbp
+	ret
+
+.globl mprotect
+.type mprotect, @function
+mprotect:
+	pushq %rbp
+	movq %rsp, %rbp
+	.equ addr, 8
+	.equ len, 12
+	.equ protect, 16
+
+	movq $10, %rax
+	syscall
+
+	movq %rbp, %rsp
+	popq %rbp
+	ret
